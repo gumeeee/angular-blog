9abfe31da72b563ffcc37533cee46335ee5c6090
gumeeee <gaxco123@gmail.com>
2023-10-16 14:36:18 -0300

    content created and database fake

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Nova atualização insana do Angular!",
+    "description": "Atualização do Angular, deixará muitos desenvolvedores de cabelo em pé! veja mais...",
+    "photoCover": "https://miro.medium.com/v2/resize:fit:783/1*Kj8Eq401fP2ecTY8r9B89Q.png"
+  },
+  {
+    "id": "2",
+    "title": "Manga de jujutsu kaisen impressiona com a morte de um personagem",
+    "description": "Toji zenin, o pai de megumi, foi morto por Gojo Satoru, o personagem mais forte do mangá de jujutsu kaisen, veja mais...",
+    "photoCover": "https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2023/07/jujutsu-kaisen-gojo-satoru-1.webp?resize=1024%2C576"
+  }
+
+
+]
